35992eca3d66a5773d716badda09f4386be240d5
dikshac1991@gmail.com <dikshac1991@gmail.com>
2021-10-26 11:38:48 -0400

    first commit

---
+++ server/index.js
@@ -0,0 +1,7 @@
+
+const config = require("./config");
+const { app, logger } = require("./server");
+
+app.listen(config.port, () => {
+  logger.info(`Listening on http://localhost:${config.port}`);
+});
